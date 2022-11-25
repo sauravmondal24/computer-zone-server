73f6dc993a1953bf8eb530f65fb48b52547b2629
sauravmondal24 <sauravmondal321@gmail.com>
2022-11-25 19:00:16 +0600

    Admin user role api create

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -21,11 +22,51 @@ async function run() {
 		const mainCategoryCollection = client
 			.db('computerZone')
 			.collection('MainCategoryData');
+		const usersCollection = client.db('computerZone').collection('users');
 
 		app.get('/mainCategory', async (req, res) => {
 			const query = {};
 			const option = await mainCategoryCollection.find(query).toArray();
 			res.send(option);
+		});
+
+		app.get('/jwt', async (req, res) => {
+			const email = req.query.email;
+			const query = { email: email };
+			const user = await usersCollection.findOne(query);
+			if (user) {
+				const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+					expiresIn: '1h'
+				});
+				return res.send({ accessToken: token });
+			}
+			console.log(user);
+			res.status(403).send({ accessToken: '' });
+		});
+
+		app.post('/users', async (req, res) => {
+			const user = req.body;
+			const result = await usersCollection.insertOne(user);
+			res.send(result);
+		});
+		app.get('/users/admin/:email', async (req, res) => {
+			const email = req.params.email;
+			const query = { email };
+			const user = await usersCollection.findOne(query);
+			res.send({ isAdmin: user?.role === 'admin' });
+		});
+
+		app.get('/seller', async (req, res) => {
+			// const role = req.params.role;
+			const query = { role: 'seller' };
+			const users = await usersCollection.find(query).toArray();
+			res.send(users);
+		});
+		app.get('/buyer', async (req, res) => {
+			// const role = req.params.role;
+			const query = { role: 'buyer' };
+			const users = await usersCollection.find(query).toArray();
+			res.send(users);
 		});
 	} finally {
 	}
